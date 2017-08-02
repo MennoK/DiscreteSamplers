820832b07f77fe5ad836db358b3a4cf94d5b5431
MennoK <menno.keustermans@student.kuleuven.be>
2017-08-02 08:34:59 +0200

    Update test.py

--- src/test.py
+++ src/test.py
@@ -3,6 +3,9 @@ import src.samplers as sp
 
 
 def test():
+    vis_samples_2D(sp.uniform_discrete_sampler_hemisphere(8, 8))
+    vis_samples_2D(sp.cosine_weighted_discrete_sampler_hemisphere(8, 8))
+    
     vis_samples_3D(sp.uniform_discrete_sampler_hemisphere(8, 8))
     vis_samples_3D(sp.cosine_weighted_discrete_sampler_hemisphere(8, 8))
 
